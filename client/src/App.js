9910cecf3a73cb55d4a2a1918255c3834ffdbc11
Md. Rakib Hossain <rakib.ba@pakiza.com>
2022-12-22 14:51:40 +0600

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import { Link, Navigate, useNavigate, Routes, Route } from "react-router-dom";
-import AdminLogin from './Authentication/AdminLogin';
-import ProtectedRoutes from './Authentication/ProtectedRoutes';
-import Dashboard from './BackendComponents/Dashboard/Dashboard';
-import PostType from './BackendComponents/PostType/PostType';
 import axios from 'axios';
-import JobConfiguration from './BackendComponents/Job Management/JobConfiguration/JobConfiguration';
-import CreateJobPost from './BackendComponents/Job Management/JobPost/CreateJobPost';
-import ViewAllJob from './BackendComponents/Job Management/JobPost/ViewAllJob';
-import ViewAllUsers from './BackendComponents/User/ViewAllUsers';
-import RoleManagement from './BackendComponents/User/RoleManagement/RoleManagement';
-import EditJobPost from './BackendComponents/Job Management/JobPost/EditJobPost';
 import './imageUrl';
-import CreateEvent from './BackendComponents/Event/CreateEvent';
-import EventConfiguration from './BackendComponents/Event/Event Configuration/EventConfiguration';
-import ViewAllEvent from './BackendComponents/Event/ViewAllEvent';
-import ViewEventPayment from './BackendComponents/ViewEventPayment/ViewEventPayment';
-import EditEvent from './BackendComponents/Event/EditEvent';
-import CreateAdvertisement from './BackendComponents/Advertisement/CreateAdvertisement';
-import ViewAllAdvertisement from './BackendComponents/Advertisement/ViewAdvertisement';
-import Settings from '../src/BackendComponents/Settings/Settings';
-import EditAdvertisement from './BackendComponents/Advertisement/EditAdvertisement';
-import AdminPasswordReset from './Authentication/AdminPasswordReset';
-import AdminPasswordResetForm from './Authentication/AdminPasswordResetForm';
+
+// import AdminLogin from './Authentication/AdminLogin';
+
+// import ProtectedRoutes from './Authentication/ProtectedRoutes';
+// import Dashboard from './BackendComponents/Dashboard/Dashboard';
+// import PostType from './BackendComponents/PostType/PostType';
+// import JobConfiguration from './BackendComponents/Job Management/JobConfiguration/JobConfiguration';
+// import CreateJobPost from './BackendComponents/Job Management/JobPost/CreateJobPost';
+// import ViewAllJob from './BackendComponents/Job Management/JobPost/ViewAllJob';
+// import ViewAllUsers from './BackendComponents/User/ViewAllUsers';
+// import RoleManagement from './BackendComponents/User/RoleManagement/RoleManagement';
+// import EditJobPost from './BackendComponents/Job Management/JobPost/EditJobPost';
+// import CreateEvent from './BackendComponents/Event/CreateEvent';
+// import EventConfiguration from './BackendComponents/Event/Event Configuration/EventConfiguration';
+// import ViewAllEvent from './BackendComponents/Event/ViewAllEvent';
+// import ViewEventPayment from './BackendComponents/ViewEventPayment/ViewEventPayment';
+// import EditEvent from './BackendComponents/Event/EditEvent';
+// import CreateAdvertisement from './BackendComponents/Advertisement/CreateAdvertisement';
+// import ViewAllAdvertisement from './BackendComponents/Advertisement/ViewAdvertisement';
+// import Settings from '../src/BackendComponents/Settings/Settings';
+// import EditAdvertisement from './BackendComponents/Advertisement/EditAdvertisement';
+// import AdminPasswordReset from './Authentication/AdminPasswordReset';
+// import AdminPasswordResetForm from './Authentication/AdminPasswordResetForm';
 
 
-import BlogArticleConfiguration from './BackendComponents/BlogArticle/BlogArticleConfiguration';
-import CreateBlogArticle from './BackendComponents/BlogArticle/CreateBlogArticle';
-import ViewBlogArticle from './BackendComponents/BlogArticle/ViewArticleBlog';
-import CreateBanner from './BackendComponents/Banner/CreateBanner';
-import ViewAllBanner from './BackendComponents/Banner/ViewBanner';
-import EditBlogArticle from './BackendComponents/BlogArticle/EditBlogArticle';
-import NoticeNewsConfiguration from './BackendComponents/NoticeNews/NoticeNewsConfiguration';
-import CreateNoticeNews from './BackendComponents/NoticeNews/CreateNoticeNews';
-import ViewNoticeNews from './BackendComponents/NoticeNews/ViewNoticeNews';
-import EditBanner from './BackendComponents/Banner/EditBanner';
-import NotFound from './BackendComponents/NotFound/NotFound';
-import UserConfiguration from './BackendComponents/User/configuration/UserConfiguration';
-import VlogConfiguration from './BackendComponents/Vlog/VlogConfiguration';
-import CreateVlog from './BackendComponents/Vlog/CreateVlog';
-import ViewVlog from './BackendComponents/Vlog/ViewVlog';
-import EditVlog from './BackendComponents/Vlog/EditVlog';
-import EditNoticeNews from './BackendComponents/NoticeNews/EditNoticeNews';
+// import BlogArticleConfiguration from './BackendComponents/BlogArticle/BlogArticleConfiguration';
+// import CreateBlogArticle from './BackendComponents/BlogArticle/CreateBlogArticle';
+// import ViewBlogArticle from './BackendComponents/BlogArticle/ViewArticleBlog';
+// import CreateBanner from './BackendComponents/Banner/CreateBanner';
+// import ViewAllBanner from './BackendComponents/Banner/ViewBanner';
+// import EditBlogArticle from './BackendComponents/BlogArticle/EditBlogArticle';
+// import NoticeNewsConfiguration from './BackendComponents/NoticeNews/NoticeNewsConfiguration';
+// import CreateNoticeNews from './BackendComponents/NoticeNews/CreateNoticeNews';
+// import ViewNoticeNews from './BackendComponents/NoticeNews/ViewNoticeNews';
+// import EditBanner from './BackendComponents/Banner/EditBanner';
+// import NotFound from './BackendComponents/NotFound/NotFound';
+// import UserConfiguration from './BackendComponents/User/configuration/UserConfiguration';
+// import VlogConfiguration from './BackendComponents/Vlog/VlogConfiguration';
+// import CreateVlog from './BackendComponents/Vlog/CreateVlog';
+// import ViewVlog from './BackendComponents/Vlog/ViewVlog';
+// import EditVlog from './BackendComponents/Vlog/EditVlog';
+// import EditNoticeNews from './BackendComponents/NoticeNews/EditNoticeNews';
+
+const AdminLogin = lazy(() => import('./Authentication/AdminLogin'));
+const ProtectedRoutes = lazy(() => import('./Authentication/ProtectedRoutes'));
+
+const Dashboard = lazy(() => import('./BackendComponents/Dashboard/Dashboard'));
+const PostType = lazy(() => import('./BackendComponents/PostType/PostType'));
+const JobConfiguration = lazy(() => import('./BackendComponents/Job Management/JobConfiguration/JobConfiguration'));
+const CreateJobPost = lazy(() => import('./BackendComponents/Job Management/JobPost/CreateJobPost'));
+const ViewAllJob = lazy(() => import('./BackendComponents/Job Management/JobPost/ViewAllJob'));
+const ViewAllUsers = lazy(() => import('./BackendComponents/User/ViewAllUsers'));
+const RoleManagement = lazy(() => import('./BackendComponents/User/RoleManagement/RoleManagement'));
+const EditJobPost = lazy(() => import('./BackendComponents/Job Management/JobPost/EditJobPost'));
+const CreateEvent = lazy(() => import('./BackendComponents/Event/CreateEvent'));
+const EventConfiguration = lazy(() => import('./BackendComponents/Event/Event Configuration/EventConfiguration'));
+const ViewAllEvent = lazy(() => import('./BackendComponents/Event/ViewAllEvent'));
+const ViewEventPayment = lazy(() => import('./BackendComponents/ViewEventPayment/ViewEventPayment'));
+const EditEvent = lazy(() => import('./BackendComponents/Event/EditEvent'));
+const CreateAdvertisement = lazy(() => import('./BackendComponents/Advertisement/CreateAdvertisement'));
+const ViewAllAdvertisement = lazy(() => import('./BackendComponents/Advertisement/ViewAdvertisement'));
+const Settings = lazy(() => import('../src/BackendComponents/Settings/Settings'));
+const EditAdvertisement = lazy(() => import('./BackendComponents/Advertisement/EditAdvertisement'));
+const AdminPasswordReset = lazy(() => import('./Authentication/AdminPasswordReset'));
+const AdminPasswordResetForm = lazy(() => import('./Authentication/AdminPasswordResetForm'));
+
+
+const BlogArticleConfiguration = lazy(() => import('./BackendComponents/BlogArticle/BlogArticleConfiguration'));
+const CreateBlogArticle = lazy(() => import('./BackendComponents/BlogArticle/CreateBlogArticle'));
+const ViewBlogArticle = lazy(() => import('./BackendComponents/BlogArticle/ViewArticleBlog'));
+const CreateBanner = lazy(() => import('./BackendComponents/Banner/CreateBanner'));
+const ViewAllBanner = lazy(() => import('./BackendComponents/Banner/ViewBanner'));
+const EditBlogArticle = lazy(() => import('./BackendComponents/BlogArticle/EditBlogArticle'));
+const NoticeNewsConfiguration = lazy(() => import('./BackendComponents/NoticeNews/NoticeNewsConfiguration'));
+const CreateNoticeNews = lazy(() => import('./BackendComponents/NoticeNews/CreateNoticeNews'));
+const ViewNoticeNews = lazy(() => import('./BackendComponents/NoticeNews/ViewNoticeNews'));
+const EditBanner = lazy(() => import('./BackendComponents/Banner/EditBanner'));
+const NotFound = lazy(() => import('./BackendComponents/NotFound/NotFound'));
+const UserConfiguration = lazy(() => import('./BackendComponents/User/configuration/UserConfiguration'));
+const VlogConfiguration = lazy(() => import('./BackendComponents/Vlog/VlogConfiguration'));
+const CreateVlog = lazy(() => import('./BackendComponents/Vlog/CreateVlog'));
+const ViewVlog = lazy(() => import('./BackendComponents/Vlog/ViewVlog'));
+const EditVlog = lazy(() => import('./BackendComponents/Vlog/EditVlog'));
+const EditNoticeNews = lazy(() => import('./BackendComponents/NoticeNews/EditNoticeNews'));
 function App() {
 
   const [storage, setStorage] = useState('');
@@ -122,104 +166,114 @@ function App() {
 
   return (
     <>
-      <Routes>
-
-      <Route path="/" element={<Navigate to='/admin-login'/>} />
-
-        <Route path="admin-login" element={<AdminLogin handleSucessLogin={handleSucessLogin} />}></Route>
-
-        <Route path="admin-password-reset-form" element={<AdminPasswordResetForm />}></Route>
-        <Route path="admin-password-reset/:id" element={<AdminPasswordReset />}></Route>
+      <Suspense fallback={<div className='text-center my-5' style={{
+        position: 'fixed', top: '0px', left: '0px', width: '100vw', height: '100vh'
 
 
+      }}>Loading...</div>}>
 
+        <Routes>
 
-        <Route element={<ProtectedRoutes />}>
-          {
-            successStatus == 'yes' ?
-              storage === 'admin' && <>
-                <Route path="admin-dashboard" element={<Dashboard />}></Route>
-              </>
+          <Route path="/" element={<Navigate to='/admin-login' />} />
 
-              :
-              <Route path="admin-login" element={<AdminLogin />}></Route>
+          <Route path="admin-login" element={<AdminLogin handleSucessLogin={handleSucessLogin} />}></Route>
 
-          }
-
-
-          <Route path="admin-dashboard" element={<Dashboard />}></Route>
-          <Route path="post-type" element={<PostType />}></Route>
-          <Route path="job-configuration" element={<JobConfiguration />}></Route>
-          <Route path="create-job-post" element={<CreateJobPost />}></Route>
-          <Route path="view-all-jobs" element={<ViewAllJob />}></Route>
-
-          <Route path="view-all-users" element={<ViewAllUsers />}></Route>
-          <Route path="role-management" element={<RoleManagement />}></Route>
-
-          <Route path="user-configuration" element={<UserConfiguration />}></Route>
+          <Route path="admin-password-reset-form" element={<AdminPasswordResetForm />}></Route>
+          <Route path="admin-password-reset/:id" element={<AdminPasswordReset />}></Route>
 
 
 
 
-          <Route path="edit-jobs/:id" element={<EditJobPost />}></Route>
+          <Route element={<ProtectedRoutes />}>
+            {
+              successStatus == 'yes' ?
+                storage === 'admin' && <>
+                  <Route path="admin-dashboard" element={<Dashboard />}></Route>
+                </>
 
-          <Route path="create-event" element={<CreateEvent />}></Route>
-          <Route path="edit-events/:id" element={<EditEvent />}></Route>
-          <Route path="event-configuration" element={<EventConfiguration />}></Route>
-          <Route path="view-all-events" element={<ViewAllEvent />}></Route>
+                :
+                <Route path="admin-login" element={<AdminLogin />}></Route>
 
-
-          <Route path="admin-dashboard" element={<Dashboard />}></Route>
-          <Route path="post-type" element={<PostType />}></Route>
-          <Route path="job-configuration" element={<JobConfiguration />}></Route>
-          <Route path="create-job-post" element={<CreateJobPost />}></Route>
-          <Route path="view-all-jobs" element={<ViewAllJob />}></Route>
-          <Route path="edit-jobs/:id" element={<EditJobPost />}></Route>
-
-          <Route path="create-event" element={<CreateEvent />}></Route>
-          <Route path="edit-events/:id" element={<EditEvent />}></Route>
-          <Route path="event-configuration" element={<EventConfiguration />}></Route>
-          <Route path="view-all-events" element={<ViewAllEvent />}></Route>
-          <Route path="view-event-payment" element={<ViewEventPayment />}></Route>
-
-          <Route path="view-blog-article" element={<ViewBlogArticle />}></Route>
-          <Route path="create-blog-article" element={<CreateBlogArticle />}></Route>
-          <Route path="edit-blog-article/:id" element={<EditBlogArticle />}></Route>
-          <Route path="blog-article-configuration" element={<BlogArticleConfiguration />}></Route>
+            }
 
 
-          <Route path="create-notice-news" element={<CreateNoticeNews />}></Route>
-          <Route path="view-notice-news" element={<ViewNoticeNews />}></Route>
-          <Route path="edit-notice-news/:id" element={<EditNoticeNews />}></Route>
-          <Route path="notice-news-configuration" element={<NoticeNewsConfiguration />}></Route>
+            <Route path="admin-dashboard" element={<Dashboard />}></Route>
+            <Route path="post-type" element={<PostType />}></Route>
+            <Route path="job-configuration" element={<JobConfiguration />}></Route>
+            <Route path="create-job-post" element={<CreateJobPost />}></Route>
+            <Route path="view-all-jobs" element={<ViewAllJob />}></Route>
+
+            <Route path="view-all-users" element={<ViewAllUsers />}></Route>
+            <Route path="role-management" element={<RoleManagement />}></Route>
+
+            <Route path="user-configuration" element={<UserConfiguration />}></Route>
 
 
 
 
-          <Route path="create-banner" element={<CreateBanner />}></Route>
-          <Route path="view-all-banner" element={<ViewAllBanner />}></Route>
-          <Route path="edit-banner/:id" element={<EditBanner />}></Route>
+            <Route path="edit-jobs/:id" element={<EditJobPost />}></Route>
+
+            <Route path="create-event" element={<CreateEvent />}></Route>
+            <Route path="edit-events/:id" element={<EditEvent />}></Route>
+            <Route path="event-configuration" element={<EventConfiguration />}></Route>
+            <Route path="view-all-events" element={<ViewAllEvent />}></Route>
+
+
+            <Route path="admin-dashboard" element={<Dashboard />}></Route>
+            <Route path="post-type" element={<PostType />}></Route>
+            <Route path="job-configuration" element={<JobConfiguration />}></Route>
+            <Route path="create-job-post" element={<CreateJobPost />}></Route>
+            <Route path="view-all-jobs" element={<ViewAllJob />}></Route>
+            <Route path="edit-jobs/:id" element={<EditJobPost />}></Route>
+
+            <Route path="create-event" element={<CreateEvent />}></Route>
+            <Route path="edit-events/:id" element={<EditEvent />}></Route>
+            <Route path="event-configuration" element={<EventConfiguration />}></Route>
+            <Route path="view-all-events" element={<ViewAllEvent />}></Route>
+            <Route path="view-event-payment" element={<ViewEventPayment />}></Route>
+
+            <Route path="view-blog-article" element={<ViewBlogArticle />}></Route>
+            <Route path="create-blog-article" element={<CreateBlogArticle />}></Route>
+            <Route path="edit-blog-article/:id" element={<EditBlogArticle />}></Route>
+            <Route path="blog-article-configuration" element={<BlogArticleConfiguration />}></Route>
+
+
+            <Route path="create-notice-news" element={<CreateNoticeNews />}></Route>
+            <Route path="view-notice-news" element={<ViewNoticeNews />}></Route>
+            <Route path="edit-notice-news/:id" element={<EditNoticeNews />}></Route>
+            <Route path="notice-news-configuration" element={<NoticeNewsConfiguration />}></Route>
 
 
 
-          <Route path="create-advertisement" element={<CreateAdvertisement />}></Route>
-          <Route path="view-all-advertisement" element={<ViewAllAdvertisement />}></Route>
-          <Route path="edit-advertisement/:id" element={<EditAdvertisement />}></Route>
 
-          <Route path="create-vlog" element={<CreateVlog />}></Route>
-          <Route path="view-vlog" element={<ViewVlog />}></Route>
-          <Route path="edit-vlog/:id" element={<EditVlog />}></Route>
-          <Route path="vlog-configuration" element={<VlogConfiguration />}></Route>
-
-          <Route path="settings" element={<Settings />}></Route>
+            <Route path="create-banner" element={<CreateBanner />}></Route>
+            <Route path="view-all-banner" element={<ViewAllBanner />}></Route>
+            <Route path="edit-banner/:id" element={<EditBanner />}></Route>
 
 
 
-        </Route>
-        <Route path="*" element={<NotFound />}></Route>
+            <Route path="create-advertisement" element={<CreateAdvertisement />}></Route>
+            <Route path="view-all-advertisement" element={<ViewAllAdvertisement />}></Route>
+            <Route path="edit-advertisement/:id" element={<EditAdvertisement />}></Route>
+
+            <Route path="create-vlog" element={<CreateVlog />}></Route>
+            <Route path="view-vlog" element={<ViewVlog />}></Route>
+            <Route path="edit-vlog/:id" element={<EditVlog />}></Route>
+            <Route path="vlog-configuration" element={<VlogConfiguration />}></Route>
+
+            <Route path="settings" element={<Settings />}></Route>
 
 
-      </Routes>
+
+          </Route>
+          <Route path="*" element={<NotFound />}></Route>
+
+
+
+
+        </Routes>
+      </Suspense>
+
 
 
     </>
--- client/src/imageUrl.js
+++ client/src/imageUrl.js
@@ -1,2 +1,2 @@
-global.img_url = 'http://172.31.120.58';
-//  global.img_url ='http://austtaa.com/server/public';
+// global.img_url = 'http://172.31.120.58';
+ global.img_url ='https://austtaa.com/server/public';
